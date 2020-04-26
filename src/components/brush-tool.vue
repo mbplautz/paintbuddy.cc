01940b0b496e33292b492260f9ba071de9b7bc7a
Michael Plautz <mplautz@lenovo.com>
2020-04-26 09:07:41 -0400

    Added touch support for brush tool

--- src/components/brush-tool.vue
+++ src/components/brush-tool.vue
@@ -14,8 +14,11 @@ import EditTool from './edit-tool.vue';
             return {
                 name: 'Brush',
                 icon: 'fas fa-paint-brush',
-                lastX: 0,
-                lastY: 0
+                mouse: {
+                    lastX: 0,
+                    lastY: 0
+                },
+                touch: {}
             }
         },
         extends: EditTool,
@@ -27,25 +30,62 @@ import EditTool from './edit-tool.vue';
                 console.log(e);
                 if (e.type === 'mousedown') {
                     let context = this.$root.paint.canvas.drawContext;
+                    // The next two lines are temporary -- until the initial and user color selection is implemented
                     context.lineWidth = this.$root.paint.options.lineWidth;
                     context.strokeStyle = this.$root.paint.options.color;
                     context.beginPath();
                     context.moveTo(e.offsetX, e.offsetY);
                     context.lineTo(e.offsetX, e.offsetY);
                     context.stroke();
-                    this.lastX = e.offsetX;
-                    this.lastY = e.offsetY;
+                    this.mouse.lastX = e.offsetX;
+                    this.mouse.lastY = e.offsetY;
+                }
+                else if (e.type === 'touchstart') {
+                    this.touch = {};
+                    let canvas = this.$root.paint.canvas.drawElement;
+                    let bounds = canvas.getBoundingClientRect();
+                    let context = this.$root.paint.canvas.drawContext;
+                    // The next two lines are temporary -- until the initial and user color selection is implemented
+                    context.lineWidth = this.$root.paint.options.lineWidth;
+                    context.strokeStyle = this.$root.paint.options.color;
+                    Array.prototype.forEach.call(e.touches, touch => {
+                        let coordinate = {
+                            x: touch.clientX - bounds.left,
+                            y: touch.clientY - bounds.top
+                        };
+                        context.beginPath();
+                        context.moveTo(coordinate.x, coordinate.y);
+                        context.lineTo(coordinate.x, coordinate.y);
+                        context.stroke();
+                        this.touch[touch.identifier] = coordinate;
+                    });
                 }
             },
             dragFunction(e) {
                 console.log(e);
                 if (e.type === 'mousemove') {
                     let context = this.$root.paint.canvas.drawContext;
-                    context.moveTo(this.lastX, this.lastY);
+                    context.moveTo(this.mouse.lastX, this.mouse.lastY);
                     context.lineTo(e.offsetX, e.offsetY);
                     context.stroke();
-                    this.lastX = e.offsetX;
-                    this.lastY = e.offsetY;
+                    this.mouse.lastX = e.offsetX;
+                    this.mouse.lastY = e.offsetY;
+                }
+                else if (e.type === 'touchmove') {
+                    let canvas = this.$root.paint.canvas.drawElement;
+                    let bounds = canvas.getBoundingClientRect();
+                    let context = this.$root.paint.canvas.drawContext;
+                    Array.prototype.forEach.call(e.touches, touch => {
+                        let coordinate = {
+                            x: touch.clientX - bounds.left,
+                            y: touch.clientY - bounds.top
+                        };
+                        context.beginPath();
+                        context.moveTo(this.touch[touch.identifier].x, this.touch[touch.identifier].y);
+                        context.lineTo(coordinate.x, coordinate.y);
+                        context.stroke();
+                        this.touch[touch.identifier] = coordinate;
+                    });
                 }
             },
             releaseFunction(e) {
