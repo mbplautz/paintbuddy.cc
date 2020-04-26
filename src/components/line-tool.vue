1e238850571e54254e06c70774e11b776053ccc6
Michael Plautz <mplautz@lenovo.com>
2020-04-26 16:02:49 -0400

    Added touch capability to line tool

--- src/components/line-tool.vue
+++ src/components/line-tool.vue
@@ -17,6 +17,12 @@ import EditTool from './edit-tool.vue';
                 mouse: {
                     initialX: 0,
                     initialY: 0
+                },
+                touch: {
+                    initialX: 0,
+                    initialY: 0,
+                    finalX: 0,
+                    finalY: 0
                 }
             }
         },
@@ -28,50 +34,79 @@ import EditTool from './edit-tool.vue';
             touchFunction(e) {
                 let canvas = this.$root.paint.canvas.activeElement;
                 let bounds = canvas.getBoundingClientRect();
+                let x, y;
                 if (e.type === 'mousedown') {
                     this.mouse.initialX = e.clientX - bounds.left;
                     this.mouse.initialY = e.clientY - bounds.top;
-                    let context = this.$root.paint.canvas.activeContext;
-                    let canvas = this.$root.paint.canvas.activeElement;
-                    context.clearRect(0, 0, canvas.width, canvas.height);
-                    // The next two lines are temporary -- until the initial and user color selection is implemented
-                    context.lineWidth = this.$root.paint.options.lineWidth;
-                    context.strokeStyle = this.$root.paint.options.color;
-                    context.beginPath();
-                    context.moveTo(this.mouse.initialX, this.mouse.initialY);
-                    context.lineTo(this.mouse.initialX, this.mouse.initialY);
-                    context.stroke();
+                    x = this.mouse.initialX;
+                    y = this.mouse.initialY;
                 }
+                else if (e.type === 'touchstart') {
+                    // For the line tool, we only use the first touch if there are multiple touches
+                    this.touch.initialX = e.touches[0].clientX - bounds.left;
+                    this.touch.initialY = e.touches[0].clientY - bounds.top;
+                    x = this.touch.initialX;
+                    y = this.touch.initialY;
+                }
+                let context = this.$root.paint.canvas.activeContext;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                // The next two lines are temporary -- until the initial and user color selection is implemented
+                context.lineWidth = this.$root.paint.options.lineWidth;
+                context.strokeStyle = this.$root.paint.options.color;
+                context.beginPath();
+                context.moveTo(x, y);
+                context.lineTo(x, y);
+                context.stroke();
             },
             dragFunction(e) {
                 let canvas = this.$root.paint.canvas.activeElement;
                 let bounds = canvas.getBoundingClientRect();
+                let initialX, initialY, currentX, currentY;
                 if (e.type === 'mousemove') {
-                    let currentX = e.clientX - bounds.left;
-                    let currentY = e.clientY - bounds.top;
-                    let context = this.$root.paint.canvas.activeContext;
-                    context.clearRect(0, 0, canvas.width, canvas.height);
-                    context.beginPath();
-                    context.moveTo(this.mouse.initialX, this.mouse.initialY);
-                    context.lineTo(currentX, currentY);
-                    context.stroke();
+                    initialX = this.mouse.initialX;
+                    initialY = this.mouse.initialY;
+                    currentX = e.clientX - bounds.left;
+                    currentY = e.clientY - bounds.top;
                 }
+                else if (e.type === 'touchmove') {
+                    initialX = this.touch.initialX;
+                    initialY = this.touch.initialY;
+                    currentX = e.touches[0].clientX - bounds.left;
+                    currentY = e.touches[0].clientY - bounds.top;
+                    this.touch.finalX = currentX;
+                    this.touch.finalY = currentY;
+                }
+                let context = this.$root.paint.canvas.activeContext;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context.beginPath();
+                context.moveTo(initialX, initialY);
+                context.lineTo(currentX, currentY);
+                context.stroke();
             },
             releaseFunction(e) {
                 let canvas = this.$root.paint.canvas.activeElement;
-                let bounds = canvas.getBoundingClientRect();
+                let initialX, initialY, finalX, finalY;
                 if (e.type === 'mouseup') {
-                    let finalX = e.clientX - bounds.left;
-                    let finalY = e.clientY - bounds.top;
-                    let context = this.$root.paint.canvas.activeContext;
-                    context.clearRect(0, 0, canvas.width, canvas.height);
-                    context = this.$root.paint.canvas.drawContext;
-                    canvas = this.$root.paint.canvas.drawElement;
-                    context.beginPath();
-                    context.moveTo(this.mouse.initialX, this.mouse.initialY);
-                    context.lineTo(finalX, finalY);
-                    context.stroke();
+                    let bounds = canvas.getBoundingClientRect();
+                    initialX = this.mouse.initialX;
+                    initialY = this.mouse.initialY;
+                    finalX = e.clientX - bounds.left;
+                    finalY = e.clientY - bounds.top;
                 }
+                else if (e.type === 'touchend') {
+                    initialX = this.touch.initialX;
+                    initialY = this.touch.initialY;
+                    finalX = this.touch.finalX;
+                    finalY = this.touch.finalY;
+                }
+                let context = this.$root.paint.canvas.activeContext;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context = this.$root.paint.canvas.drawContext;
+                canvas = this.$root.paint.canvas.drawElement;
+                context.beginPath();
+                context.moveTo(initialX, initialY);
+                context.lineTo(finalX, finalY);
+                context.stroke();
             }
         }
     }
